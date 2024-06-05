d3e6efcb8fb437a7cf61c45a1bff62dc9017fb3c
Thomaz Alves <thomazhilario5@gmail.com>
2024-06-05 14:03:56 -0300

    feat: Card.js
    - Passar as propriedades recebidas do componentes, para as tags html.

--- organo/src/components/Card/index.js
+++ organo/src/components/Card/index.js
@@ -2,11 +2,11 @@ export const Card = ({nome, cargo, imagem}) => {
     return(
         <div className='card'>
             <div className='top__card'>
-                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaAycHruKZaIUk9wA5baKNnQr9IMcQNIhtRA&s' alt='imagem do colaborador'/>
+                <img src={imagem} alt='imagem do colaborador'/>
             </div>
             <div className='bottom__card'>
-                <h2>Thomaz Alves</h2>
-                <h3>Desenvolvedor React na Alura</h3>
+                <h2>{nome}</h2>
+                <h3>{cargo}</h3>
             </div>
         </div>
     )
