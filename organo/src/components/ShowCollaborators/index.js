70b997b18ce2e533cf5023ca81754f494e846451
Thomaz Alves <thomazhilario5@gmail.com>
2024-06-05 13:34:00 -0300

    feat: ShowCollaborators.js
    - Renderizar o componente com base no valor do booleano.

--- organo/src/components/ShowCollaborators/index.js
+++ organo/src/components/ShowCollaborators/index.js
@@ -5,12 +5,16 @@ import './showCollaborators.css'
 
 export const ShowCollaborators = ({times, colaboradores}) => {
 
-    return(
-        <section className='container__colaboradores'>
-            <h1>Minha Organização:</h1>
-            <hr/>
+    const collaboratornNotIsEmpty = colaboradores.length > 0
 
-            {times.map((time, index) => <ContainerTeam key={index} title={time} team={colaboradores.filter(colaborador => colaborador.team === time)}/>)}
-        </section>
-    )
+    if(collaboratornNotIsEmpty){
+        return(
+            <section className='container__colaboradores'>
+                <h1>Minha Organização:</h1>
+                <hr/>
+    
+                {times.map((time, index) => <ContainerTeam key={index} title={time} team={colaboradores.filter(colaborador => colaborador.team === time)}/>)}
+            </section>
+        )
+    }
 }
