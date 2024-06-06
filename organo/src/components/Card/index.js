20dbf80e71f46b3d33a7e8789cb0c1139181cacb
Thomaz Alves <thomazhilario5@gmail.com>
2024-06-06 14:56:55 -0300

    feat: Card.js
    - Estilizar o card com o icone de exclusão.
    - Alocar a função que deleta um card específico.
    - Corrigindo o css do Card.

--- organo/src/components/Card/index.js
+++ organo/src/components/Card/index.js
@@ -1,6 +1,10 @@
+// lucide react
+import { BadgeX } from 'lucide-react'
+
 export const Card = ({nome, cargo, imagem, corPrincipal, teamColaborador ,deletarColaborador}) => {
     return(
         <div className='card'>
+            <BadgeX className='delete__card' size={30} onClick={() => deletarColaborador(nome, teamColaborador)}/>
             <div className='top__card' style={{backgroundColor:corPrincipal}}>
                 <img src={imagem} alt='imagem do colaborador'/>
             </div>
