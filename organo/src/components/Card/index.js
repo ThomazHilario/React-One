95048d69355aa8e9c01ea5338984defa27216680
Thomaz Alves <thomazhilario5@gmail.com>
2024-06-10 16:11:48 -0300

    feat: Card.js
    - Favoritar um card.

--- organo/src/components/Card/index.js
+++ organo/src/components/Card/index.js
@@ -1,7 +1,12 @@
+import { useState } from 'react'
+
 // lucide react
-import { BadgeX } from 'lucide-react'
+import { BadgeX, Heart } from 'lucide-react'
 
 export const Card = ({nome, cargo, imagem, corPrincipal, teamColaborador ,deletarColaborador}) => {
+
+    const [isMyFavoriteCard, setIsMyFavoriteCard] = useState(false)
+
     return(
         <div className='card'>
             <BadgeX className='delete__card' size={30} onClick={() => deletarColaborador(nome, teamColaborador)}/>
@@ -11,6 +16,11 @@ export const Card = ({nome, cargo, imagem, corPrincipal, teamColaborador ,deleta
             <div className='bottom__card'>
                 <h2>{nome}</h2>
                 <h3>{cargo}</h3>
+                <Heart 
+                    size={20}
+                    color={isMyFavoriteCard ? 'red' : 'black'}
+                    onClick={() => setIsMyFavoriteCard(!isMyFavoriteCard)}
+                />
             </div>
         </div>
     )
