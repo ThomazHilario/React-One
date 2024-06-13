680801c35cbab43311cc9e1bf7b5b08789b7acb0
Thomaz Alves <thomazhilario5@gmail.com>
2024-06-13 14:10:04 -0300

    feat: Error.jsx
    - Criar a pagina notFound.

--- ola-mundo/src/pages/Error/index.jsx
+++ ola-mundo/src/pages/Error/index.jsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom'
+
+// css import
+import './error.css'
+
 export const Error = () => {
-    return <h1>Ops! esta pagina nao existe!!!</h1>
+    return (
+        <div className='container__error'>
+            <h1>Error 404</h1>
+
+            <h2>Página não encontrada!!</h2>
+
+            <Link to='/'>Voltar</Link>
+        </div>
+    )
 }
